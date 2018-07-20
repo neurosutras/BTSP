d636402cca9160cb1b6f5de9aad21ac084569d9a
Aaron D. Milstein <neurosutras@gmail.com>
2018-07-20 14:04:13 -0700

    20180720:  - Added analysis of parameter cross-correlations and explanation of variance (pca).

--- plot_BTSP_model_param_distributions.py
+++ plot_BTSP_model_param_distributions.py
@@ -4,6 +4,8 @@ from nested.optimize_utils import *
 import matplotlib as mpl
 import matplotlib.gridspec as gridspec
 from collections import defaultdict
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
 import click
 
 
@@ -25,14 +27,16 @@ context = Context()
 @click.option("--config-file-path", type=click.Path(exists=True, file_okay=True, dir_okay=False),
               default='config/optimize_BTSP_CA1_cli_config.yaml')
 @click.option("--output-dir", type=click.Path(exists=True, file_okay=False, dir_okay=True), default='data')
+@click.option("--pcadim", type=int, default=None)
 @click.option("--export", is_flag=True)
 @click.option("--label", type=str, default=None)
-def main(param_file_path, config_file_path, output_dir, export, label):
+def main(param_file_path, config_file_path, output_dir, pcadim, export, label):
     """
 
     :param param_file_path: str (path)
     :param config_file_path: str (path)
     :param output_dir: str (dir)
+    :param pcadim: int (number or pca components to plot)
     :param export: bool
     :param label: str
     """
@@ -45,7 +49,8 @@ def main(param_file_path, config_file_path, output_dir, export, label):
         raise IOError('Invalid param_file_path: %s' % param_file_path)
     if export and not os.path.isdir(output_dir):
         raise IOError('Invalid output_dir: %s' % output_dir)
-    plot_BTSP_model_param_cdfs(param_file_path, config_file_path, export, output_dir, label)
+    # plot_BTSP_model_param_cdfs(param_file_path, config_file_path, export, output_dir, label)
+    plot_BTSP_model_param_PCA(param_file_path, pcadim, export, output_dir, label)
     context.update(locals())
 
 
@@ -69,16 +74,16 @@ def plot_BTSP_model_param_cdfs(param_file_path, config_file_path, export=False, 
                     'local_signal_decay': 'local signal tau$_{decay}$ (ms)',
                     'global_signal_rise': 'global signal tau$_{rise}$ (ms)',
                     'global_signal_decay': 'global signal tau$_{decay}$ (ms)',
-                    'peak_delta_weight': 'max synaptic weight',
+                    'peak_delta_weight': 'weight$_{max}$',
                     'rMC0': 'k$_{pot}$ (Hz)',
-                    'rMC_th': 'rate$_{pot}$ th',
-                    'rMC_peak': 'rate$_{pot}$ xpeak',
+                    'rMC_th': 'rate$_{pot}$ th$_{0}$',
+                    'rMC_peak': 'rate$_{pot}$ xpeak$_{0}$',
                     'rCM0': u'k$_{de\u2011pot}$ (Hz)',
-                    'rCM_th1': u'rate$_{de\u2011pot}$ th1',
-                    'rCM_peak1': u'rate$_{de\u2011pot}$ xpeak1',
-                    'rCM_th2': u'rate$_{de\u2011pot}$ th2',
-                    'rCM_peak2': u'rate$_{de\u2011pot}$ xpeak2',
-                    'rCM_min2': u'rate$_{de\u2011pot}$ ymin2'}
+                    'rCM_th1': u'rate$_{de\u2011pot}$ th$_{1}$',
+                    'rCM_peak1': u'rate$_{de\u2011pot}$ xpeak$_{1}$',
+                    'rCM_th2': u'rate$_{de\u2011pot}$ th$_{2}$',
+                    'rCM_peak2': u'rate$_{de\u2011pot}$ xpeak$_{2}$',
+                    'rCM_min2': u'rate$_{de\u2011pot}$ ymin$_{2}$'}
     bounds = read_from_yaml(config_file_path)['bounds']
 
     for cell in cell_keys:
@@ -114,6 +119,96 @@ def plot_BTSP_model_param_cdfs(param_file_path, config_file_path, export=False, 
     if export:
         fig_path = '%s/%s_BTSP_model_param_cdfs.svg' % (output_dir, label)
         fig.savefig(fig_path, format='svg')
+    plt.show()
+
+
+def plot_BTSP_model_param_PCA(param_file_path, pcadim=None, export=False, output_dir=None, label=None):
+    """
+
+    :param param_file_path: str (path)
+    :param pcadim: int (number of pca components to plot)
+    :param export: bool
+    :param output_dir: str (dir)
+    :param label: str
+    :return: dict
+    """
+    params_by_cell = read_from_yaml(param_file_path)
+    ordered_param_keys = ['local_signal_rise', 'local_signal_decay', 'global_signal_rise', 'global_signal_decay',
+                          'peak_delta_weight', 'rMC0', 'rMC_th', 'rMC_peak', 'rCM0', 'rCM_th1', 'rCM_peak1', 'rCM_th2',
+                          'rCM_peak2', 'rCM_min2']
+    param_labels = {'local_signal_rise': 'local signal tau$_{rise}$',
+                    'local_signal_decay': 'local signal tau$_{decay}$',
+                    'global_signal_rise': 'global signal tau$_{rise}$',
+                    'global_signal_decay': 'global signal tau$_{decay}$',
+                    'peak_delta_weight': 'weight$_{max}$',
+                    'rMC0': 'k$_{pot}$',
+                    'rMC_th': 'rate$_{pot}$ th$_{0}$',
+                    'rMC_peak': 'rate$_{pot}$ xpeak$_{0}$',
+                    'rCM0': u'k$_{de\u2011pot}$ (Hz)',
+                    'rCM_th1': u'rate$_{de\u2011pot}$ th$_{1}$',
+                    'rCM_peak1': u'rate$_{de\u2011pot}$ xpeak$_{1}$',
+                    'rCM_th2': u'rate$_{de\u2011pot}$ th$_{2}$',
+                    'rCM_peak2': u'rate$_{de\u2011pot}$ xpeak$_{2}$',
+                    'rCM_min2': u'rate$_{de\u2011pot}$ ymin$_{2}$'}
+    data = []
+    for cell, param_dict in params_by_cell.iteritems():
+        if cell not in ['all']:
+            data.append(param_dict_to_array(param_dict, ordered_param_keys))
+    data = np.array(data)
+    scaled_data = scale(data)
+    corr = np.corrcoef(scaled_data, rowvar=False)
+
+    pca = PCA(whiten=False)  # , n_components=dim)
+    fit = pca.fit(scaled_data)
+    print 'Explained variance ratios: '
+    print pca.explained_variance_ratio_
+    # print pca.components_
+    trans_data = fit.transform(scaled_data)
+    plot_data = trans_data
+
+    if pcadim is None:
+        pcadim = pca.n_components_-1
+    else:
+        pcadim = min(pcadim, pca.n_components_-1)
+    plotdim = pcadim -1
+    context.update(locals())
+
+    x = np.arange(len(ordered_param_keys) + 1)
+    y = np.arange(len(ordered_param_keys) + 1)
+    labels = [param_labels[key] for key in ordered_param_keys]
+    X, Y = np.meshgrid(x, y[::-1])
+    fig, axes = plt.subplots(figsize=(12, 10))
+    pc = axes.pcolor(X, Y, corr)
+    axes.set_xticks(np.arange(len(ordered_param_keys)) + 0.5)
+    axes.set_yticks(np.arange(len(ordered_param_keys)) + 0.5)
+    axes.set_xticklabels(labels, ha='right', fontsize=16)
+    axes.set_yticklabels(labels[::-1], fontsize=16)
+    for tick in axes.get_xticklabels():
+        tick.set_rotation(45.)
+    fig.colorbar(pc)
+    clean_axes(axes)
+    fig.tight_layout()
+    if export:
+        fig_path = '%s/%s_BTSP_model_param_corrcoefs.svg' % (output_dir, label)
+        fig.savefig(fig_path, format='svg')
+    plt.show()
+
+    if plotdim == 1:
+        fig, axes = plt.subplots(figsize=(5, 5))
+        axes.scatter(plot_data[:, 0], plot_data[:, 1])
+        axes.set_ylabel('PC2')
+        axes.set_xlabel('PC1')
+    else:
+        fig, axes = plt.subplots(plotdim, plotdim, figsize=(10, 10))
+        for row in xrange(plotdim):
+            axes[row][0].set_ylabel('PC%i' % (row + 2))
+            for col in xrange(plotdim):
+                if row == plotdim - 1:
+                    axes[row][col].set_xlabel('PC%i' % (col + 1))
+                if row >= col:
+                    axes[row][col].scatter(plot_data[:, col], plot_data[:, row + 1])
+    clean_axes(axes)
+    fig.tight_layout()
     plt.show()
 
 
