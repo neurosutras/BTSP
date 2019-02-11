ebc93b50d2813a766ef54474551cb94b5e0f70b7
Aaron D. Milstein <neurosutras@gmail.com>
2019-02-11 12:28:16 -0800

    20190211: - BTSP_D and BTSP_E should now appropriately discard models that fluctuate weights across laps without improving residuals.

--- jobscripts/cori_optimize_BTSP_E_CA1_submit.sh
+++ jobscripts/cori_optimize_BTSP_E_CA1_submit.sh
@@ -4,10 +4,10 @@ cd $HOME/BTSP
 
 for i in 11 28 29 30 31 8;
 do
-    bash jobscripts/cori_optimize_BTSP_E_CA1_cli_debug.sh $i 7
+    bash jobscripts/cori_optimize_BTSP_E_CA1_cli.sh $i 7
 done
 
 for i in 1 15 18 4 5 7 9;
 do
-    bash jobscripts/cori_optimize_BTSP_E_CA1_cli_debug.sh $i 13
+    bash jobscripts/cori_optimize_BTSP_E_CA1_cli.sh $i 13
 done
