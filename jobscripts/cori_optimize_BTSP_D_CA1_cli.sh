12dd27c6e2eaf4be19ab27b349b0ef9d05e302f1
Aaron D. Milstein <neurosutras@gmail.com>
2019-02-15 16:15:50 -0800

    20190215: rev3: Adding cori jobscripts

---
+++ jobscripts/cori_optimize_BTSP_D_CA1_cli.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+export JOB_NAME=optimize_BTSP_D_CA1_cell"$1"_"$DATE"
+export DATE=$(date +%Y%m%d_%H%M%S)
+export cores=$(($2 * 32))
+sbatch <<EOT
+#!/bin/bash -l
+#SBATCH -J $JOB_NAME
+#SBATCH -o /global/cscratch1/sd/aaronmil/BTSP/logs/"$JOB_NAME".%j.o
+#SBATCH -e /global/cscratch1/sd/aaronmil/BTSP/logs/"$JOB_NAME".%j.e
+#SBATCH -q regular
+#SBATCH -N $2
+#SBATCH -L SCRATCH
+#SBATCH -C haswell
+#SBATCH -t 06:00:00
+#SBATCH --mail-user=aaronmil@stanford.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+set -x
+
+cd $HOME/BTSP
+
+srun -N $2 -n $cores -c 2 --cpu_bind=cores python -m nested.optimize \
+    --config-file-path=config/optimize_BTSP_D_CA1_cli_config.yaml --disp --output-dir=$SCRATCH/BTSP \
+    --pop-size=200 --max-iter=50 --path-length=3 --disp --export --label=cell"$1" --cell_id=$1
+EOT
