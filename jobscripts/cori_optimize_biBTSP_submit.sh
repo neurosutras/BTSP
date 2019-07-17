a128aed723c27d6595d0a85e45a53d363e8afe6d
Aaron D. Milstein <neurosutras@gmail.com>
2019-07-17 10:42:07 -0700

    20190717:  - Preparing to optimize all alternative models on all cells on Cori.

--- jobscripts/cori_optimize_biBTSP_submit.sh
+++ jobscripts/cori_optimize_biBTSP_submit.sh
@@ -4,10 +4,10 @@ cd $HOME/BTSP
 
 for i in 11 28 29 30 31 8;
 do
-    bash jobscripts/cori_optimize_biBTSP_cli.sh $i 7 $2 $3
+    bash jobscripts/cori_optimize_biBTSP_cli.sh $i 7 $1 $2
 done
 
 for i in 15 18 4 5 7 9;  # 1
 do
-    bash jobscripts/cori_optimize_biBTSP_cli.sh $i 13 $2 $3
+    bash jobscripts/cori_optimize_biBTSP_cli.sh $i 13 $1 $2
 done
---
+++ jobscripts/cori_optimize_biBTSP_submit_debug3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+cd $HOME/BTSP
+
+#for i in 11 28 29 30 31 8;
+#do
+#    bash jobscripts/cori_optimize_biBTSP_cli_debug.sh $i 7 $1 $2
+#done
+
+for i in 7 9;  # 1 15 18 4 5
+do
+    bash jobscripts/cori_optimize_biBTSP_cli_debug.sh $i 13 $1 $2
+done
