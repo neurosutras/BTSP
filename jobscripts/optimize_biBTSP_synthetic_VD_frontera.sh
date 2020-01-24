bb7b3dd1e84ca54ddbe00551904ed11c33b6bdba
Aaron D. Milstein <neurosutras@gmail.com>
2020-01-24 12:10:50 -0800

    20200124:  - Preparing to run some variants of the network simulations on Frontera.

---
+++ jobscripts/optimize_biBTSP_synthetic_VD_frontera.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+export DATE=$(date +%Y%m%d_%H%M%S)
+export JOB_NAME=optimize_biBTSP_synthetic_VD_"$DATE"
+sbatch <<EOT
+#!/bin/bash -l
+#SBATCH -J $JOB_NAME
+#SBATCH -o /scratch1/06441/aaronmil/src/BTSP/logs/$JOB_NAME.%j.o
+#SBATCH -e /scratch1/06441/aaronmil/src/BTSP/logs/$JOB_NAME.%j.e
+#SBATCH -p normal
+#SBATCH -N 8
+#SBATCH -n 448
+#SBATCH -t 6:00:00
+#SBATCH --mail-user=aaronmil@stanford.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+set -x
+
+cd $SCRATCH/src/BTSP
+
+ibrun -n 448 python3 -m nested.optimize --config-file-path=config/optimize_biBTSP_synthetic_VD_config.yaml \
+    --output-dir=data --pop_size=200 --max_iter=50 --path_length=3 --disp --export
+EOT
