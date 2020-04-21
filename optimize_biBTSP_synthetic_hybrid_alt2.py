49c97e0b985fd6f48cfa3d43040a760df71f975a
Aaron D. Milstein <neurosutras@gmail.com>
2020-04-21 14:59:57 -0700

    20200421:  - Testing more alternative versions of the VD and WD models (D).

--- optimize_biBTSP_synthetic_hybrid_alt2.py
+++ optimize_biBTSP_synthetic_hybrid_alt2.py
@@ -892,10 +892,10 @@ def plot_model_summary_figure(export_file_path=None, exported_data_key=None, ind
     dep_rate = np.vectorize(scaled_single_sigmoid(
         context.f_dep_th, context.f_dep_th + context.f_dep_half_width, signal_xrange))
     phi_pot = np.vectorize(scaled_single_sigmoid(
-        context.vd_th, context.vd_th + context.vd_half_width, vrange, [context.vd_pot_min, 1.]))
+        context.vd_th, context.vd_pot_th + context.vd_half_width, vrange, [context.vd_pot_min, 1.]))
     # phi_dep = np.vectorize(lambda x: 1.)
     phi_dep = np.vectorize(scaled_single_sigmoid(
-        context.vd_th, context.vd_th + context.vd_half_width, vrange, [context.vd_dep_min, 1.]))
+        context.vd_th, context.vd_dep_th + context.vd_half_width, vrange, [context.vd_dep_min, 1.]))
     # phi_dep = phi_pot
 
     fig, axes = plt.subplots(1, 3, figsize=(10, 3.5))
