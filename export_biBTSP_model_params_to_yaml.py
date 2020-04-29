4db27960bee7b8177378b55c8190595bc3bbea30
Aaron D. Milstein <neurosutras@gmail.com>
2020-04-29 15:24:52 -0700

    20200429:  - Adding some convenience utils to process large volumes of optimization results.

--- export_biBTSP_model_params_to_yaml.py
+++ export_biBTSP_model_params_to_yaml.py
@@ -4,26 +4,25 @@ import click
 
 
 @click.command()
-@click.option("--legend-file-name", type=click.Path(exists=True, file_okay=True, dir_okay=False),
+@click.option("--legend-file-path", type=click.Path(exists=True, file_okay=True, dir_okay=False),
               required=True)
-@click.option("--export-file-name", type=str)
-@click.option("--output-dir", type=click.Path(exists=True, file_okay=False, dir_okay=True), default='config')
+@click.option("--export-file-path", type=str)
+@click.option("--data-dir", type=click.Path(exists=True, file_okay=False, dir_okay=True), default='data')
 @click.option("--verbose", is_flag=True)
-def main(legend_file_name, export_file_name, output_dir, verbose):
+def main(legend_file_path, export_file_path, data_dir, verbose):
     """
 
-    :param legend_file_name: str (path)
-    :param export_file_name: str
-    :param output_dir: str (dir path)
+    :param legend_file_path: str (path)
+    :param export_file_path: str
+    :param data_dir: str (dir path)
     :param verbose: bool
     """
-    if not os.path.isdir(output_dir):
-        raise IOError('Invalid output_dir: %s' % output_dir)
-    export_file_path = '%s/%s' % (output_dir, export_file_name)
+    if not os.path.isdir(data_dir):
+        raise IOError('Invalid data_dir: %s' % data_dir)
     model_params = dict()
-    legend = read_from_yaml(legend_file_name)
+    legend = read_from_yaml(legend_file_path)
     for key, file_name in legend.items():
-        file_path = '%s/%s' % (output_dir, file_name)
+        file_path = '%s/%s' % (data_dir, file_name)
         report = OptimizationReport(file_path=file_path)
         model_params[key] = param_array_to_dict(report.survivors[0].x, report.param_names)
     if verbose:
