20200526:  - Fixing filter normalization.

optimize_biBTSP_WD_E.py
@@ -292,7 +292,7 @@ def compute_features_signal_amplitudes(x, cell_id=None, induction=None, model_id
         get_dual_exp_decay_signal_filters(context.local_signal_decay, context.global_signal_decay,
                                           context.down_dt, plot)
     global_signal = get_global_signal(context.down_induction_gate, global_filter)
-    local_signals = get_local_signal_population(local_signal_filter, context.down_rate_maps, context.down_dt)
+    local_signals = get_local_signal_population(local_signal_filter, context.down_rate_maps)
 
     result = {'local_signal_peak': np.max(local_signals),
               'global_signal_peak': np.max(global_signal)}
@@ -382,7 +382,7 @@ def calculate_model_ramp(local_signal_peak=None, global_signal_peak=None, model_
                                           context.down_dt, plot)
     global_signal = np.divide(get_global_signal(context.down_induction_gate, global_filter), global_signal_peak)
     local_signals = \
-        np.divide(get_local_signal_population(local_signal_filter, context.down_rate_maps, context.down_dt),
+        np.divide(get_local_signal_population(local_signal_filter, context.down_rate_maps),
                   local_signal_peak)
 
     signal_xrange = np.linspace(0., 1., 10000)
@@ -740,7 +740,7 @@ def plot_model_summary_supp_figure(cell_id, export_file_path=None, exported_data
 
     global_signal = np.divide(get_global_signal(context.down_induction_gate, global_filter), global_signal_peak)
     local_signals = \
-        np.divide(get_local_signal_population(local_signal_filter, context.down_rate_maps, context.down_dt),
+        np.divide(get_local_signal_population(local_signal_filter, context.down_rate_maps),
                   local_signal_peak)
 
     signal_xrange = np.linspace(0., 1., 10000)
@@ -1139,7 +1139,7 @@ def plot_model_summary_figure(cell_id, export_file_path=None, exported_data_key=
 
     global_signal = np.divide(get_global_signal(context.down_induction_gate, global_filter), global_signal_peak)
     local_signals = \
-        np.divide(get_local_signal_population(local_signal_filter, context.down_rate_maps, context.down_dt),
+        np.divide(get_local_signal_population(local_signal_filter, context.down_rate_maps),
                   local_signal_peak)
 
     signal_xrange = np.linspace(0., 1., 10000)

optimize_biBTSP_synthetic_hybrid.py
@@ -452,8 +452,8 @@ def calculate_model_ramp(model_id=None, export=False, plot=False):
     global_signal = get_global_signal(context.down_induction_gate, global_filter)
     global_signal_peak = np.max(global_signal)
     global_signal /= global_signal_peak
-    local_signal_peak = np.max(get_local_signal_population(local_signal_filter, context.down_rate_maps,
-                                                           context.down_dt))
+    local_signal_peak = np.max(get_local_signal_population(local_signal_filter, context.down_rate_maps /
+                                                           context.input_field_peak_rate))
 
     signal_xrange = np.linspace(0., 1., 10000)
     dend_depo_range = np.linspace(context.min_dend_depo, context.max_dend_depo, 10000)
@@ -571,9 +571,9 @@ def calculate_model_ramp(model_id=None, export=False, plot=False):
         for i, (this_rate_map, this_current_normalized_weight) in \
                 enumerate(zip(context.down_rate_maps, current_normalized_weights)):
             this_normalized_rate_map = this_rate_map / context.input_field_peak_rate
-            this_spine_depo = get_spine_depo(this_normalized_rate_map) * context.input_field_peak_rate
+            this_spine_depo = get_spine_depo(this_normalized_rate_map)
             this_local_signal = \
-                np.divide(get_local_signal(this_spine_depo, local_signal_filter, context.down_dt), local_signal_peak)
+                np.divide(get_local_signal(this_spine_depo, local_signal_filter), local_signal_peak)
             this_signal_overlap = np.multiply(this_local_signal[indexes], global_signal[indexes])
             this_pot_rate = np.trapz(pot_rate(this_signal_overlap), dx=context.down_dt / 1000.)
             this_dep_rate = np.trapz(dep_rate(this_signal_overlap), dx=context.down_dt / 1000.)